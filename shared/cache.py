7b7c9b3806a69f720b125b2b8ee8cb5a56a91a42
Alan B <death1027@outlook.com>
2024-08-18 15:54:21 -0600

    refactor: :recycle: add tls to redis

--- shared/cache.py
+++ shared/cache.py
@@ -5,6 +5,7 @@ REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = os.getenv("REDIS_PORT", 6379)
 REDIS_DB = os.getenv("REDIS_DB", 0)
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
+REDIS_TLS = os.getenv("REDIS_TLS", "False") == "True"
 cache = None
 
 
@@ -12,7 +13,11 @@ def init_redis():
     global cache
     try:
         cache = Redis(
-            host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD
+            host=REDIS_HOST,
+            port=REDIS_PORT,
+            db=REDIS_DB,
+            password=REDIS_PASSWORD,
+            ssl=REDIS_TLS,
         )
         if Redis.ping(cache):
             print("Redis is running")
